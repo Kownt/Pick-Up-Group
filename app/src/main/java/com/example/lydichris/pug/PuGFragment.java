bd3cced0694ff5151a0cd6a82da5c80ae0806ac9
cgmcandrews <cgmcandrews@wpi.edu>
2015-06-14 17:00:27 -0400

    Initial Comm with empty project.

---
+++ app/src/main/java/com/example/lydichris/pug/PuGFragment.java
@@ -0,0 +1,23 @@
+package com.example.lydichris.pug;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class PuGFragment extends Fragment {
+
+    public PuGFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_pu_g, container, false);
+    }
+}
